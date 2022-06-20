6471b19866cb55cc5d3e6ad7542c4c63b4b2bd83
Chengyf <1351416566@qq.com>
2022-06-20 16:19:21 +0800

    feat: 完成数据处理

--- handleQQChatLog.py
+++ handleQQChatLog.py
@@ -1,14 +1,37 @@
 import xlsxwriter
 import openpyxl
 
-xb = openpyxl.load_workbook('1-12.xlsx')
+# xb = openpyxl.load_workbook('1-12.xlsx')
+xb = openpyxl.load_workbook('13-23.xlsx')
 
-A1 = xb['！A1']
+# sheet = xb['！A1']
+# sheet = xb['！A2']
+# sheet = xb['A3']
+# sheet = xb['A4']
+# sheet = xb['A5']
+# sheet = xb['A6']
+# sheet = xb['B7']
+# sheet = xb['B8']
+# sheet = xb['B9']
+# sheet = xb['B10']
+# sheet = xb['B11']
+# sheet = xb['B12']
+# sheet = xb['13组']
+# sheet = xb['14组']
+# sheet = xb['15组']
+# sheet = xb['16组']
+# sheet = xb['17组']
+# sheet = xb['18组']
+# sheet = xb['19组']
+# sheet = xb['20组']
+# sheet = xb['21组']
+# sheet = xb['22组']
+sheet = xb['23组']
 
 original_list = []
 
-for i in range(1, 900):
-    original_list.append(str(A1.cell(row=i, column=1).value).replace('\xa0', ' '))
+for i in range(1, 5000):
+    original_list.append(str(sheet.cell(row=i, column=1).value).replace('\xa0', ' '))
 
 # print(a)
 
@@ -22,15 +45,15 @@ for i in range(0, len(original_list)):
     elif original_list[i].find('2022') == -1:
         s += original_list[i] + ' '
     else:
-        temp = original_list[i].split(' ')
+        temp = original_list[i].split(' ', 1)
         for j in range(0, len(temp)):
             first_handle_list.append(temp[j])
 # print(first_handle_list)
 
 second_handle_list = []
 for i in range(0, len(first_handle_list)):
-    if first_handle_list[i] != '' and first_handle_list[i].find('撤回了一条消息') == -1 and \
-            first_handle_list[i].find('修改了群名称') == -1:
+    if first_handle_list[i] != '' and first_handle_list[i].find('撤回了') == -1 and \
+            first_handle_list[i].find('修改了群名称') == -1 and first_handle_list[i].find('点击添加好友') == -1:
         second_handle_list.append(first_handle_list[i])
 # print(second_handle_list)
 
@@ -43,10 +66,54 @@ print(final_handle_list)
 
 
 # 创建工作簿
-file_name = "demo.xlsx"
+# file_name = "！A1.xlsx"
+# file_name = "！A2.xlsx"
+# file_name = "A3.xlsx"
+# file_name = "A4.xlsx"
+# file_name = "A5.xlsx"
+# file_name = "A6.xlsx"
+# file_name = "B7.xlsx"
+# file_name = "B8.xlsx"
+# file_name = "B9.xlsx"
+# file_name = "B10.xlsx"
+# file_name = "B11.xlsx"
+# file_name = "B12.xlsx"
+# file_name = "13组.xlsx"
+# file_name = "14组.xlsx"
+# file_name = "15组.xlsx"
+# file_name = "16组.xlsx"
+# file_name = "17组.xlsx"
+# file_name = "18组.xlsx"
+# file_name = "19组.xlsx"
+# file_name = "20组.xlsx"
+# file_name = "21组.xlsx"
+# file_name = "22组.xlsx"
+file_name = "23组.xlsx"
 workbook = xlsxwriter.Workbook(file_name)
 # 创建工作表
-worksheet = workbook.add_worksheet('first_sheet1')
+# worksheet = workbook.add_worksheet('！A1')
+# worksheet = workbook.add_worksheet('！A2')
+# worksheet = workbook.add_worksheet('A3')
+# worksheet = workbook.add_worksheet('A4')
+# worksheet = workbook.add_worksheet('A5')
+# worksheet = workbook.add_worksheet('A6')
+# worksheet = workbook.add_worksheet('B7')
+# worksheet = workbook.add_worksheet('B8')
+# worksheet = workbook.add_worksheet('B9')
+# worksheet = workbook.add_worksheet('B10')
+# worksheet = workbook.add_worksheet('B11')
+# worksheet = workbook.add_worksheet('B12')
+# worksheet = workbook.add_worksheet('13组')
+# worksheet = workbook.add_worksheet('14组')
+# worksheet = workbook.add_worksheet('15组')
+# worksheet = workbook.add_worksheet('16组')
+# worksheet = workbook.add_worksheet('17组')
+# worksheet = workbook.add_worksheet('18组')
+# worksheet = workbook.add_worksheet('19组')
+# worksheet = workbook.add_worksheet('20组')
+# worksheet = workbook.add_worksheet('21组')
+# worksheet = workbook.add_worksheet('22组')
+worksheet = workbook.add_worksheet('23组')
 
 line = []
 row = 0
